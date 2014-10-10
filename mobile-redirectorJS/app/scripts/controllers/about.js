c4cad86d51343a54c2e47582a105bb32bee2f480
victor dias <victor.dias@ipanemax.com>
2014-10-10 14:10:07 -0300

    add mobileBox

---
+++ mobile-redirectorJS/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mobileRedirectorJsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mobileRedirectorJsApp
+ */
+angular.module('mobileRedirectorJsApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ mobile-redirectorJS/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mobileRedirectorJsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mobileRedirectorJsApp
+ */
+angular.module('mobileRedirectorJsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
