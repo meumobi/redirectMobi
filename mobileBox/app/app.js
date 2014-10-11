2c689ef9da951c18e7a94a432c1f56f2b58b544d
victor dias <victor.dias@ipanemax.com>
2014-10-10 21:01:41 -0300

    update content to use siemens s headlines

--- mobileBox/app/app.js
+++ mobileBox/app/app.js
@@ -96,19 +96,19 @@ app.controller('MainController', function($rootScope, $scope, analytics){
 
   for (var i=1; i<=100; i++) {
     scrollItems.push({
-			img: "http://enterprise.meumobilesite.com/uploads/items/314x220_6752235.jpg",
-			caption: "Nullam sapien massa, aliquam in cursus ut, ullamcorper in tortor. Aliquam mauris arcu,... ",
-			title: "Excellent feature!"
+			img: "http://www.siemens.com/pool/newselement/br/siemens-in-brazil.jpg",
+			caption: "No Brasil, atualmente contamos com aproximadamente 8 mil colaboradores, 13 fábricas e 7 centros de P&D. Assista ao vídeo e conheça tudo que a Siemens pode oferecer nas áreas de Automação, digitalização e eletrificação.",
+			title: "A presença da Siemens no Brasil"
 		});
     scrollItems.push({
-			img: "http://enterprise.meumobilesite.com/uploads/items/314x220_6764296.jpg",
-			caption: "Nullam sapien massa, aliquam in cursus ut, ullamcorper in tortor. Aliquam mauris arcu.Nullam sapien massa, aliquam in cursus ut, ullamcorper in tortor. Aliquam mauris arcu ",
-			title: "Aluna nota mil fatura dando dicas pela web"
+			img: "http://www.siemens.com/pool/newselement/br/dow-jones-sustainability-index_268x130.jpg",
+			caption: "A Siemens é a empresa mais sustentável no setor industrial, de acordo com o Índice de Dow Jones de Sustentabilidade (Dow Jones Sustainability Index – DJSI). A Siemens ficou em primeiro lugar dentre as empresas do setor Bens de Capital do ranking que reúne cerca de 350 empresas com melhores práticas de sustentabilidade em todo o mundo em sete setores de atividade econômica.",
+			title: "Siemens lidera o ranking do Índice Dow Jones de Sustentabilidade"
 		});
     scrollItems.push({
-			img: "http://s2.glbimg.com/jMHSgVE_D3dudW2-VogPePSptsA=/s.glbimg.com/jo/g1/f/original/2014/10/10/000_was8871493.jpg",
-			caption: "Regiões ativas, com mais luz e energia, formam espécie de ‘rosto’. Ondas dourada e amarela de campos magnéticos contribuem para o efeito.",
-			title: "Sol vira 'abóbora de Haloween' em foto divulgada pela Nasa"
+			img: "http://www.siemens.com/pool/newselement/br/siemens-brazil-our-commitment-to-integrity_268x130.jpg",
+			caption: "Desde 2007, a Siemens tem consistentemente aplicado um rigoroso sistema de transparência e integridade (conhecido internacionalmente como Sistema de Compliance). Este sistema visa a garantir o atendimento a normas, códigos, leis e procedimentos, a fim de combater desvios e fraudes organizacionais, oferecendo maior transparência aos seus acionistas e tornando-se referência mundial em ética nos negócios. O compromisso da Siemens contra más condutas não se refere somente ao presente e ao futuro, mas também ao passado.",
+			title: "Nosso Compromisso com a Integridade"
 		});
   }
 
